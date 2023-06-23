42992290e244473f0f5b013519a78eafc13eebe6
Gaurav Yadav <gary391@gmail.com>
2023-06-22 20:52:59 -0700

    Way to hardcode the AWS generated logicalID with a specific string, this will avoid you running into issue of resource already exist if you updated the logicalId.

--- cdk-stater/lib/photsStack.ts
+++ cdk-stater/lib/photsStack.ts
@@ -6,15 +6,19 @@ export class PhotosStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
       super(scope, id, props);
  
-      const myBucket =  new Bucket(this, 'PhotosBucket3', {
+      const myBucket =  new Bucket(this, 'PhotosBucket4', {
         bucketName: 'photosbucket-234kj3465'
       });
 
       // Using this command you can avoid running into an issue where it says the
-      // resource already exists. 
+      // resource already exists. This is being done, but assigning a specific logical id
+      // name of the bucket. Thus even if you change the stack Id, the below command 
+      // will override the AWS generated logicalId with this hardcoded value. 
       
       (myBucket.node.defaultChild as CfnBucket).overrideLogicalId('PhotoBucket2234kj34')
+
       // Create a new resource 
       // delete the old one 
+
     }
 }
